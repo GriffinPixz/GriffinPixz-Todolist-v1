86d6ef4ef57efb757e9dff8954e8f838d2227385
GriffinPixz <griffinpixz@gmail.com>
2022-02-28 02:48:24 +0700

    mark complete to do list item(s)

--- app.js
+++ app.js
@@ -6,48 +6,75 @@ const port = 8000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 
 // var toDoItem = "";
-var toDoItems = [
+let toDoItems = [
+    "Make Unicorn Tail smoothie for breakfast",
+    "Go to Mr. McGriffith class's at 13:00 AM, North Pole Hall",
     "Study \"Moon Magic & Psychic Mediumship\" tonight",
-    "Go to class \"How To Fly Efficiently With Magic Broom\" with Professor Nicholas M. McGriffith at 13:31 AM",
-
+    // "Go to class \"How To Fly Efficiently With Magic Broom\" with Professor Nicholas M. McGriffith at 13:31 AM",
 ];
+let workToDoItems = [];
+// let historyToDoItems = [];
 
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
+    let today = new Date();
 
     // Get only day name
     // var options = { weekday: 'long' };
 
     // Get full date long format
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     
-    var day = today.toLocaleDateString("en-US", options)
+    let day = today.toLocaleDateString("en-US", options)
 
     res.render("index", { newDay: day, toDoListItems: toDoItems });
+    // res.render("index", { listTitle: day, toDoListItems: toDoItems });
 
     // console.log("✅ Everything works perfectly. GriffinPixz caught a GET request to \"/\".");
 
 });
 
-
 app.post("/", (req, res) => { 
 
-    var htmlData = req.body;
+    let htmlData = req.body;
     console.log("* data value => " + htmlData.newItem);
 
-    var toDoItem = htmlData.newItem;
+    let toDoItem = htmlData.newItem;
     toDoItems.push(toDoItem);
 
     res.redirect("/");
 
+    // document.querySelector('li.todo-list-item').on('click', function (e) {
+    //     let target = e.target;
+    //     historyToDoItems.push(target);
+    //     target.hide();
+
+    //     console.log(historyToDoItems);
+
+    //     // if (target.is('li.todo-list-item')) {
+    //     //     target.hide();
+    //     // }
+    // });
 });
+
+
+app.get('/worklist', (req, res) => { 
+    res.render("index", { listTitle: "Work To Do List", toDoListItems: workToDoItems });
+});
+app.post('/worklist', (req, res) => { 
+    let workData = req.body;
+    let workToDoItems = workData.newItem;
+    workToDoItems.push(workToDoItems);
+
+    res.redirect("/worklist");
+});
+
 
 
 app.listen(port, () => { 
